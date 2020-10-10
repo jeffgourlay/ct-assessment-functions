d6ee265611e3cce5b291e023d48df5b206b60089
jeffgourlay <jeffgourlay@gmail.com>
2020-10-10 00:15:39 -0400

    add result formatting

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -11,7 +11,7 @@ let firstMulplicand = 0;
 let secondAddend = 0;
 let secondMultiplicand = 0;
 let factorialInput = document.querySelector("#factorial");
-console.log(factorialInput)
+let fibonacciInput = document.querySelector("#fibonacci");
 let firstAddendInput = document.querySelector("#first-addend");
 let firstExponentInput = document.querySelector('#number');
 let firstMultInput = document.querySelector("#first-multiplicand");
@@ -21,6 +21,7 @@ let secondMultInput = document.querySelector("#second-multiplicand");
 let doAdditionButton = document.querySelector("#do-addition");
 let doExponentButton = document.querySelector("#do-exponent");
 let doFactorialButton = document.querySelector("#do-factorial");
+let doFibonacciButton = document.querySelector("#do-fibonacci");
 let doMultiplicationButton = document.querySelector('#do-multiplication');
 
 /*
@@ -28,19 +29,19 @@ Event listeners
 */
 doAdditionButton.addEventListener("click", function() {
     let sum = add(Number(firstAddendInput.value), Number(secondAddendInput.value));
-    document.getElementById("add-result").innerHTML = sum;
-})
+    document.getElementById("add-result").innerHTML = sum.toLocaleString();
+});
 
 doMultiplicationButton.addEventListener("click", function() {
     let product = multiply(Number(firstMultInput.value),Number(secondMultInput.value));
-    document.getElementById("multiply-result").innerHTML = product;
-})
+    document.getElementById("multiply-result").innerHTML = product.toLocaleString();
+});
 
 doExponentButton.addEventListener("click", function() {
     let result = power(Number(firstExponentInput.value), Number(secondExponentInput.value));
-    document.getElementById("power-result").innerHTML = result;
+    document.getElementById("power-result").innerHTML = result.toLocaleString();
 
-})
+});
 
 doFactorialButton.addEventListener("click", function() {
     if (Number(factorialInput.value) < 1) {
@@ -48,8 +49,19 @@ doFactorialButton.addEventListener("click", function() {
     }
     else {
         let result = factorial(Number(factorialInput.value));
-        document.getElementById("factorial-result").innerHTML = result;
+        document.getElementById("factorial-result").innerHTML = result.toLocaleString();
     }
+});
+
+doFibonacciButton.addEventListener("click", function() {
+    if (Number(fibonacciInput.value) < 1) {
+        alert("Please choose a number greater than 0");
+    }
+    else {
+        let result = fibonacci(Number(fibonacciInput.value));
+        document.getElementById("fibonacci-result").innerHTML = result.toLocaleString();
+    }
+
 })
 
 /*
@@ -69,9 +81,8 @@ function multiply(firstNum, secondNum) {
     }
 
     // Fix up the sign
-    // I hope this isn't cheating since all I'm doing is correcting the sign of the result
     if ((firstNum > 0 && secondNum < 0) || (firstNum < 0 && secondNum > 0)) {
-        multResult = multResult * -1;
+        multResult = -multResult;
     }
     console.log("result: " + multResult)
     return(multResult);
@@ -108,4 +119,35 @@ function factorial(number) {
     }
 
     return (result);
+}
+
+function fibonacci(number) {
+    let firstFibonaci = 0;
+    let loop = 0;
+    let secondFibonacci = 1;
+    let result = 0;
+
+    console.log(number);
+    if (number === 1) {
+        result = firstFibonaci;
+        console.log("desired index = 1");
+    }
+    else if (number === 2) {
+        result = secondFibonacci;
+        console.log("desired index = 2");
+    }
+    else {
+        loop = 3;
+        console.log("desired index = " + number);
+        while (loop <= number) {
+            result = add(firstFibonaci, secondFibonacci);
+            firstFibonaci = secondFibonacci;
+            secondFibonacci = result;
+            console.log("First number = " + firstFibonaci + " second number = " + secondFibonacci)
+            loop++;
+        }
+    }
+
+    return (result);
+
 }
